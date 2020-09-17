7482705d8b4928f82e1dbc55f59fe94d92c58573
Mateusz <62012360+fizyczny@users.noreply.github.com>
2020-09-17 18:34:13 +0200

    Add files via upload

---
+++ alfa/build/CMakeFiles/PET.dir/DependInfo.cmake
@@ -0,0 +1,59 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/local1/Desktop/alfa/PET.cc" "/home/local1/Desktop/alfa/build/CMakeFiles/PET.dir/PET.cc.o"
+  "/home/local1/Desktop/alfa/src/DetectorConstruction.cc" "/home/local1/Desktop/alfa/build/CMakeFiles/PET.dir/src/DetectorConstruction.cc.o"
+  "/home/local1/Desktop/alfa/src/Dish.cc" "/home/local1/Desktop/alfa/build/CMakeFiles/PET.dir/src/Dish.cc.o"
+  "/home/local1/Desktop/alfa/src/EMPhysics.cc" "/home/local1/Desktop/alfa/build/CMakeFiles/PET.dir/src/EMPhysics.cc.o"
+  "/home/local1/Desktop/alfa/src/EventAction.cc" "/home/local1/Desktop/alfa/build/CMakeFiles/PET.dir/src/EventAction.cc.o"
+  "/home/local1/Desktop/alfa/src/GeneralPhysics.cc" "/home/local1/Desktop/alfa/build/CMakeFiles/PET.dir/src/GeneralPhysics.cc.o"
+  "/home/local1/Desktop/alfa/src/OutputRoot.cc" "/home/local1/Desktop/alfa/build/CMakeFiles/PET.dir/src/OutputRoot.cc.o"
+  "/home/local1/Desktop/alfa/src/PhysicsList.cc" "/home/local1/Desktop/alfa/build/CMakeFiles/PET.dir/src/PhysicsList.cc.o"
+  "/home/local1/Desktop/alfa/src/PrimaryGeneratorAction.cc" "/home/local1/Desktop/alfa/build/CMakeFiles/PET.dir/src/PrimaryGeneratorAction.cc.o"
+  "/home/local1/Desktop/alfa/src/RunAction.cc" "/home/local1/Desktop/alfa/build/CMakeFiles/PET.dir/src/RunAction.cc.o"
+  "/home/local1/Desktop/alfa/src/Source.cc" "/home/local1/Desktop/alfa/build/CMakeFiles/PET.dir/src/Source.cc.o"
+  "/home/local1/Desktop/alfa/src/SteppingAction.cc" "/home/local1/Desktop/alfa/build/CMakeFiles/PET.dir/src/SteppingAction.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "G4INTY_USE_QT"
+  "G4INTY_USE_XT"
+  "G4LIB_BUILD_DLL"
+  "G4MULTITHREADED"
+  "G4UI_USE"
+  "G4UI_USE_QT"
+  "G4UI_USE_TCSH"
+  "G4UI_USE_XM"
+  "G4VERBOSE"
+  "G4VIS_USE"
+  "G4VIS_USE_OPENGL"
+  "G4VIS_USE_OPENGLQT"
+  "G4VIS_USE_OPENGLX"
+  "G4VIS_USE_OPENGLXM"
+  "G4_STORE_TRAJECTORY"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/clhep/2.4.1.0/lib/CLHEP-2.4.1.0/../../include"
+  "../include"
+  "/usr/local/root/6.14.06/include/root"
+  "/usr/local/geant4.10.05.p01/include/Geant4"
+  "/usr/local/XercesC/3.2.2/include"
+  "/usr/include/QtCore"
+  "/usr/include/QtGui"
+  "/usr/include/QtOpenGL"
+  "/usr/local/clhep/2.4.1.0/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ alfa/build/CMakeFiles/PET.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PET.dir/PET.cc.o"
+  "CMakeFiles/PET.dir/src/DetectorConstruction.cc.o"
+  "CMakeFiles/PET.dir/src/Dish.cc.o"
+  "CMakeFiles/PET.dir/src/EMPhysics.cc.o"
+  "CMakeFiles/PET.dir/src/EventAction.cc.o"
+  "CMakeFiles/PET.dir/src/GeneralPhysics.cc.o"
+  "CMakeFiles/PET.dir/src/OutputRoot.cc.o"
+  "CMakeFiles/PET.dir/src/PhysicsList.cc.o"
+  "CMakeFiles/PET.dir/src/PrimaryGeneratorAction.cc.o"
+  "CMakeFiles/PET.dir/src/RunAction.cc.o"
+  "CMakeFiles/PET.dir/src/Source.cc.o"
+  "CMakeFiles/PET.dir/src/SteppingAction.cc.o"
+  "PET.pdb"
+  "PET"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PET.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
